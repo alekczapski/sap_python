9626aca22afdf582cd0bbc211ff3333cf6289a6c
Aleksander Czapski <16439248+alekczapski@users.noreply.github.com>
2018-05-08 19:32:58 +0200

    Fixed Python 2 compatibility issues

--- sap_gui_1.py
+++ sap_gui_1.py
@@ -12,22 +12,22 @@ def main():
     try:
 
         SapGuiAuto = win32com.client.GetObject("SAPGUI")
-        if not type(SapGuiAuto) == win32com.client.CDispatch:
+        if not isinstance(SapGuiAuto, win32com.client.CDispatch):
             return
 
         application = SapGuiAuto.GetScriptingEngine
-        if not type(application) == win32com.client.CDispatch:
+        if not isinstance(application, win32com.client.CDispatch):
             SapGuiAuto = None
             return
 
         connection = application.Children(0)
-        if not type(connection) == win32com.client.CDispatch:
+        if not isinstance(connection, win32com.client.CDispatch):
             application = None
             SapGuiAuto = None
             return
 
         session = connection.Children(0)
-        if not type(session) == win32com.client.CDispatch:
+        if not isinstance(session, win32com.client.CDispatch):
             connection = None
             application = None
             SapGuiAuto = None
